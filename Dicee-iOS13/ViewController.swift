8f4940bae419115939df64bd1e6af5a4f5c5163b
Alex Pereira de Oliveira <alexpkodi@gmail.com>
2020-03-31 21:38:33 -0300

    Created Dice Roll game

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5;
+
+    
+
+    //Action from rollbutton to change dice images
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        
+        //Give a random elemento from array
+        diceImageView1.image = diceArray.randomElement()
+        //Random number from 0 to 5
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
     }
-
-
 }
 
